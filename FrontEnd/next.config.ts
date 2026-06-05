c1df719fa0c599b8c4b71f446bc8000deccda132
Noibisjunior <noibisjunior22@gmail.com>
2026-06-05 16:02:09 +0200

    updated

--- FrontEnd/next.config.ts
+++ FrontEnd/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://demo.masteringbackend.com/:path*",
+      },
+    ];
+  },
 };
-
 export default nextConfig;
